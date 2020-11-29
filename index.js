ae86961e1a6c7ec11933bc96375f58d89bf32c1a
Jessica Mangiameli <jmangiam@asu.edu>
2020-11-29 13:23:37 -0600

    questions

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdow');
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the title of your project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project:"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process:"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project used for?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who is contributing to this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Are there any tests included?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project:",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your Github username:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email:"
+        }
+    ]);
+};
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
+        const answers = await promptUser();
+        const generateContent = generateMarkdown(answers);
 
-}
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('Successfully wrote to READMe.md');
+    } catch(err) {
+        console.log(err);
+    }
+};
 
 // function call to initialize program
 init();
